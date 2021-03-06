a7ad92bdd21090d51d59398d4ab72113f5e57c39
Mohammad Touhidul Islam <muhdtohidul@gmail.com>
2021-03-06 10:38:34 +0600

    django Templates

---
+++ MyFirstProject/FirstApp/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from django.urls import path
+from FirstApp import views
+
+
+urlpatterns=[
+    path('', views.index, name='index')
+]
--- MyFirstProject/MyFirstProject/wsgi.py
+++ MyFirstProject/MyFirstProject/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for MyFirstApp project.
+WSGI config for MyFirstProject project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyFirstApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyFirstProject.settings')
 
 application = get_wsgi_application()
